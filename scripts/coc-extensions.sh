ff6c84bc851882c71c591ece81ac499a4e6f2727
Daniel Long <danlong5@gmail.com>
2020-12-07 11:36:27 -0600

    installing python coc extension

--- scripts/coc-extensions.sh
+++ scripts/coc-extensions.sh
@@ -1,3 +1,3 @@
 nvim +PluginInstall +qall
 
-nvim -c "CocInstall coc-tsserver coc-html coc-json coc-emmet coc-css coc-git coc-yaml"
+nvim -c "CocInstall coc-tsserver coc-html coc-json coc-emmet coc-css coc-git coc-yaml coc-python"
